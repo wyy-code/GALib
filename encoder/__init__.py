f06793f151b534ce58d6cf6f8065f8167cf39ace
wyy-code <54620490+wyy-code@users.noreply.github.com>
2023-06-12 13:32:08 +0800

    Add files via upload

---
+++ encoder/__init__.py
@@ -0,0 +1,10 @@
+# from encoder.BigAlign import BigAlign
+# from encoder.DeepLink import DeepLink
+# from encoder.FINAL import FINAL
+# from encoder.IONE import IONE
+# from encoder.IsoRank import IsoRank
+# from encoder.REGAL import REGAL
+# from encoder.PALE import PALE
+
+# __all__ = ['BigAlign', 'DeepLink', 'FINAL', 'IONE', 'IsoRank', 'REGAL', 'PALE']
+
