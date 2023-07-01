b9061d3f8b8a97828bd98daa139ad074533382df
wyy-code <54620490+wyy-code@users.noreply.github.com>
2023-07-01 13:10:59 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,12 +5,12 @@
 # --alignmethod REGAL \
 # --refinemethod RefiNA 
 
-# python main.py \
-# --true_align data/arenas/arenas_edges-mapping-permutation.txt \
-# --combined_graph data/arenas/arenas_combined_edges.txt \
-# --embmethod xnetMF \
-# --alignmethod CONE \
-# --refinemethod RefiNA 
+python main.py \
+--true_align data/arenas/arenas_edges-mapping-permutation.txt \
+--combined_graph data/arenas/arenas_combined_edges.txt \
+--embmethod xnetMF \
+--alignmethod CONE \
+--refinemethod RefiNA 
 
 # python main.py \
 # --true_align data/arenas/arenas_edges-mapping-permutation.txt \
