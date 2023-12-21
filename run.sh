042030917c2c0deff46cb4cd9c63cdb720ee2d2b
Leo Wang <wangyuanyi713@gmail.com>
2023-12-21 19:11:52 +0800

    Update run

--- run.sh
+++ run.sh
@@ -1,25 +1,25 @@
-# python main.py \
+# python run.py \
 # --true_align data/arenas/arenas_edges-mapping-permutation.txt \
 # --combined_graph data/arenas/arenas_combined_edges.txt \
 # --embmethod xnetMF \
 # --alignmethod REGAL \
 # --refinemethod RefiNA 
 
-python main.py \
+python run.py \
 --true_align data/arenas/arenas_edges-mapping-permutation.txt \
 --combined_graph data/arenas/arenas_combined_edges.txt \
 --embmethod xnetMF \
 --alignmethod CONE \
 --refinemethod RefiNA 
 
-# python main.py \
+# python run.py \
 # --true_align data/arenas/arenas_edges-mapping-permutation.txt \
 # --combined_graph data/arenas/arenas_combined_edges.txt \
 # --embmethod gwl \
 # --alignmethod gwl \
 # --refinemethod RefiNA
 
-# python main.py \
+# python run.py \
 # --true_align data/arenas/arenas_edges-mapping-permutation.txt \
 # --combined_graph data/arenas/arenas_combined_edges.txt \
 # --embmethod xnetMF \
